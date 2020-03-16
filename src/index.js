112322b88bcf14f17b93863749c686ed39f9c095
albertcastaned <albcastar@gmail.com>
2020-03-15 22:09:57 -0600

    Hacer tutorial, hacer disenio e implementarlo en la pagina

--- src/index.js
+++ src/index.js
@@ -1,39 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-
-class Square extends React.Component {
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-    render() {
-      return (
-        <button 
-            className="square" 
-            onClick={() => this.setState({value: 'X'}) }
-        >
-          {this.state.value}
-        </button>
-      );
-    }
+  function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
   
   class Board extends React.Component {
+
+
     renderSquare(i) {
-      return <Square value={i}/>;
+      return (
+        <Square
+          value={this.props.squares[i]}
+          onClick={() => this.props.onClick(i)}
+        />
+      );
     }
   
-    render() {
-      const status = 'Next player: X';
-  
+    render() {      
+
       return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -53,23 +49,127 @@ class Square extends React.Component {
       );
     }
   }
-  
+
   class Game extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        history: [{
+          squares: Array(9).fill(null),
+        }],
+        stepNumber: 0,
+        xIsNext: true,
+      };
+    }
+
+    handleClick(i) {
+
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+      if(calculateWinner(squares) || squares[i])
+      {
+        return;
+      }
+
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState(
+        {
+          history: history.concat([{
+            squares: squares,
+          }]),
+          stepNumber: history.length,
+          xIsNext: !this.state.xIsNext,
+        }
+      );
+    }
+
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      });
+    }
     render() {
+      const history = this.state.history;
+      const current = history[this.state.stepNumber];
+      const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+        const desc = move ?
+          'Go to move #' + move :
+          'Go to game start';
+        return (
+          <li key={move}>
+            <Button variant="contained" color="primary" onClick={() => this.jumpTo(move)}>{desc}</Button>
+          </li>
+        );
+      });
+      let status;
+
+      if(winner)
+      {
+        status = 'Winner: ' + winner;
+      }else{
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
       return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
+
+        <div className="app">
+          <NavBar />
+          <div className="title">
+          <Typography variant="h4">
+            {status}
+          </Typography>
           </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+
+          <div className="game">
+            <div className="game-board">
+              <Board
+                squares={current.squares}
+                onClick={(i) => this.handleClick(i)} 
+              />
+            </div>
+            <div className="game-info">
+              <ol>{moves}</ol>
+            </div>
           </div>
         </div>
+
       );
     }
   }
-  
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  function NavBar() {
+    return (
+    <AppBar position="static" >
+      <Toolbar>
+        <Typography variant="h6">
+          Tic Tac Toe
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    );
+  }
   // ========================================
   
   ReactDOM.render(
